6388a12ef806e536a150c2bebdda7b77dce62f18
KunalP99 <kunal_patel200@hotmail.co.uk>
2022-09-08 16:42:42 +0100

    Added more weather information

--- src/weatherInformation.js
+++ src/weatherInformation.js
@@ -7,28 +7,36 @@ async function getLocation() {
   let lon;
   const api = 'dd190da1025e91ba1feede45a0752686';
 
+  const weatherIcon = document.querySelector('.weather-icon');
   const cityElement = document.getElementById('location');
   const tempElement = document.getElementById('temp');
-  const weatherIcon = document.querySelector('.weather-icon');
+  const windElement = document.getElementById('wind-speed');
+  const humidityElement = document.getElementById('humidity');
+  const feelsLikeElement = document.getElementById('feels-like');
 
   navigator.geolocation.getCurrentPosition((position) => {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
 
-    const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}`;
+    const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}&units=metric`;
 
     fetch(base)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        console.log(data.weather[0].icon);
         const { temp } = data.main;
-        const celcius = temp - 273.15;
+        const windSpeed = data.wind.speed;
+        const { humidity } = data.main;
+        const feelsLike = data.main.feels_like;
+
         // const farenehit = 1.8 * (temp - 273.15) + 32;
 
-        tempElement.textContent = `${Math.round(celcius)} °C`;
-        cityElement.textContent = data.name;
         weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        tempElement.textContent = `${Math.round(temp)} °C`;
+        cityElement.textContent = data.name;
+        windElement.textContent = `${windSpeed} m/s`;
+        humidityElement.textContent = `${humidity}%`;
+        feelsLikeElement.textContent = `${feelsLike} °C`;
       });
   });
 }
